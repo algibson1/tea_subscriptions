80aa0b362abb8d16a1d522a787613bc0fe7ea7bb
Artemy Gibson <artemygibson@gmail.com>
2023-10-31 12:49:40 -0700

    feat: generate customers table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_31_192756) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_31_194836) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "street_address"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "teas", force: :cascade do |t|
     t.string "blend_name"
