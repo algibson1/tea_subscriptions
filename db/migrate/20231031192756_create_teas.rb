70ccfa9f6d95405fed520e304b68ab0937585506
Artemy Gibson <artemygibson@gmail.com>
2023-10-31 12:37:02 -0700

    feat: create teas table in database and generate associated model

---
+++ db/migrate/20231031192756_create_teas.rb
@@ -0,0 +1,13 @@
+class CreateTeas < ActiveRecord::Migration[7.0]
+  def change
+    create_table :teas do |t|
+      t.string :blend_name
+      t.integer :type
+      t.string :description
+      t.integer :brew_temperature
+      t.integer :brew_time
+
+      t.timestamps
+    end
+  end
+end
