99422eceb645f9d7419fc18b761ddf034c9044fc
Artemy Gibson <artemygibson@gmail.com>
2023-10-31 21:17:25 -0700

    feat: add index for customer_subscriptions

--- app/controllers/api/v1/customer_subscriptions_controller.rb
+++ app/controllers/api/v1/customer_subscriptions_controller.rb
@@ -1,13 +1,18 @@
 class Api::V1::CustomerSubscriptionsController < ApplicationController 
+  def index 
+    customer = Customer.find(params[:customer_id])
+    render json: CustomerSubscriptionsSerializer.new(customer).to_json
+  end
+
   def create 
     CustomerSubscription.create!(cust_sub_params)
     render json: {message: "Successfully subscribed!"}, status: 200
   end
 
-  def destroy 
-    sub = CustomerSubscription.find_by(cust_sub_params)
-    sub.destroy
-  end
+  # def destroy 
+  #   sub = CustomerSubscription.find_by(cust_sub_params)
+  #   sub.destroy
+  # end
 
   private 
 
