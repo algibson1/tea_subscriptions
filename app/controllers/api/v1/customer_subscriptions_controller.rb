bc6540672bfcac408524bd16bfe629b700f47a9d
Artemy Gibson <artemygibson@gmail.com>
2023-11-01 12:46:18 -0700

    refactor: Add before actions in CustomerSubscriptionsController to find customer and subscription

--- app/controllers/api/v1/customer_subscriptions_controller.rb
+++ app/controllers/api/v1/customer_subscriptions_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::CustomerSubscriptionsController < ApplicationController 
+  before_action :find_customer
+  before_action :find_subscription, only: [:create, :update]
+  
   def index 
-    customer = Customer.find(params[:customer_id])
-    render json: CustomerSubscriptionsSerializer.new(customer).to_json
+    render json: CustomerSubscriptionsSerializer.new(@customer).to_json
   end
 
   def create 
@@ -10,14 +12,24 @@ class Api::V1::CustomerSubscriptionsController < ApplicationController
   end
 
   def update 
-    sub =  CustomerSubscription.find_by(customer_id: params[:customer_id], subscription_id: params[:id])
+    sub = CustomerSubscription.find_by(customer: @customer, subscription: @subscription)
     sub.update(status: params[:status])
-    render json: {message: "Subscription Cancelled"}
+    if sub.status == "Cancelled"
+      render json: {message: "Subscription Cancelled"}
+    end
   end
 
   private 
 
   def cust_sub_params 
     params.permit(:subscription_id, :customer_id)
+  end
+
+  def find_customer
+    @customer = Customer.find(params[:customer_id])
+  end
+
+  def find_subscription
+    @subscription = Subscription.find(params[:subscription_id] || params[:id])
   end
 end
