11fb34f97615e183abd41456d7bbd4c8eb62ca0b
Artemy Gibson <artemygibson@gmail.com>
2023-11-01 10:13:40 -0700

    feat: Add validation for unique records of active subscriptions

--- app/models/customer_subscription.rb
+++ app/models/customer_subscription.rb
@@ -1,6 +1,16 @@
 class CustomerSubscription < ApplicationRecord
   belongs_to :customer
   belongs_to :subscription
+  validate :unique_or_inactive
 
   enum status: %w(Active Cancelled Paused)
+
+  def unique_or_inactive
+    found = CustomerSubscription.find_by(customer_id: self.customer_id, subscription_id: self.subscription_id)
+    if !found.nil? && found.status == "Active"
+      errors.add(:customer, "is already subscribed!")
+    elsif !found.nil? && found.status != "Active"
+      found.destroy
+    end
+  end
 end
--- spec/models/customer_subscription_spec.rb
+++ spec/models/customer_subscription_spec.rb
@@ -5,4 +5,29 @@ RSpec.describe CustomerSubscription, type: :model do
     it { should belong_to :customer }
     it { should belong_to :subscription }
   end
+
+  describe "validations" do
+    context "an association between customer and subscription already exists" do
+      it "throws error if customer subscription is already active" do
+        @sally = Customer.create!(first_name: "Sally", last_name: "Johnson", email: "example@test.com", street_address: "1234 Some Street", city: "Littleton", state: "CO", zip: "80125")
+        @black = Subscription.create!(name: "Black Tea Pack", price: 20.37, frequency: "Monthly")
+        CustomerSubscription.create!(customer: @sally, subscription: @black, status: "Active")
+
+        expect { CustomerSubscription.create!(customer: @sally, subscription: @black) }.to raise_error("Validation failed: Customer is already subscribed!")
+      end
+
+      it "Replaces old subscription if subscription is not active" do
+        @sally = Customer.create!(first_name: "Sally", last_name: "Johnson", email: "example@test.com", street_address: "1234 Some Street", city: "Littleton", state: "CO", zip: "80125")
+        @black = Subscription.create!(name: "Black Tea Pack", price: 20.37, frequency: "Monthly")
+        CustomerSubscription.create!(customer: @sally, subscription: @black, status: "Cancelled")
+
+        expect(CustomerSubscription.count).to eq(1)
+        expect(CustomerSubscription.last.status).to eq("Cancelled")
+
+        CustomerSubscription.create!(customer: @sally, subscription: @black)
+        expect(CustomerSubscription.count).to eq(1)
+        expect(CustomerSubscription.last.status).to eq("Active")
+      end
+    end
+  end
 end
