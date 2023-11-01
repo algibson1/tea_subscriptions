c56bd748fb321898487c2fa7a6f5a42a10c7378f
Artemy Gibson <artemygibson@gmail.com>
2023-10-31 21:17:08 -0700

    feat: add route to index of a customer's subscriptions

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do 
       resources :customers do
-        resource :subscriptions, only: [:create, :destroy], controller: :customer_subscriptions
+        resources :subscriptions, only: [:index, :create, :update], controller: :customer_subscriptions
       end
     end
   end
