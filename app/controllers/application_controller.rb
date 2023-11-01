b2778559c0a7f828f9391261772b376f5b3abb15
Artemy Gibson <artemygibson@gmail.com>
2023-11-01 09:08:47 -0700

    feat: Add error handling for RecordNotFound errors to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+
+  def not_found_response(error)
+    render json: ErrorSerializer.serialize(error), status: :not_found
+  end
 end
