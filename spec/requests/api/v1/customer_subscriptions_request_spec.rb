6613219cbdc6a2e925128060e261db59629cc98f
Artemy Gibson <artemygibson@gmail.com>
2023-11-01 13:12:33 -0700

    test: Add sad path testing for CustomerSubscription update endpoint

--- spec/requests/api/v1/customer_subscriptions_request_spec.rb
+++ spec/requests/api/v1/customer_subscriptions_request_spec.rb
@@ -176,26 +176,89 @@ RSpec.describe "Customer Subscriptions Endpoints" do
         expect(subscriptions2.first[:attributes][:status]).to eq("Cancelled")
       end
 
-      xit "can pause a subscription" do
+      it "can pause a subscription" do
+        load_test_data
 
+        get "/api/v1/customers/#{@sally.id}/subscriptions"
+        subscriptions1 = JSON.parse(response.body, symbolize_names: true)[:data]
+        expect(subscriptions1.first[:id]).to eq(@black.id)
+        expect(subscriptions1.first[:attributes][:status]).to eq("Active")
+
+        patch "/api/v1/customers/#{@sally.id}/subscriptions/#{@black.id}", params: {status: "Paused"}
+
+        expect(response).to be_successful
+        expect(response.status).to eq(200)
+        message = JSON.parse(response.body, symbolize_names: true)[:message]
+        expect(message).to eq("Subscription Paused")
+
+        get "/api/v1/customers/#{@sally.id}/subscriptions"
+        subscriptions2 = JSON.parse(response.body, symbolize_names: true)[:data]
+        expect(subscriptions2.first[:id]).to eq(@black.id)
+        expect(subscriptions2.first[:attributes][:status]).to eq("Paused")
       end
 
-      xit "returns an error if association does not exist" do
-        
+      it "returns an error if association does not exist" do
+        load_test_data
+
+        patch "/api/v1/customers/#{@junior.id}/subscriptions/#{@black.id}", params: {status: "Cancelled"}
+
+        expect(response).to_not be_successful
+        expect(response.status).to eq(422)
+        error = JSON.parse(response.body, symbolize_names: true)
+        expected = {
+          errors: [
+            detail: "Junior has no subscription to Black Tea Pack that can be updated"
+          ]
+        }
+        expect(error).to eq(expected)
       end
 
-      xit "throws an error if the given status is not a valid status" do
+      it "throws an error if the given status is not a valid status" do
+        load_test_data
 
+        patch "/api/v1/customers/#{@sally.id}/subscriptions/#{@black.id}", params: {status: "Something invalid"}
+
+        expect(response).to_not be_successful
+        expect(response.status).to eq(422)
+        error = JSON.parse(response.body, symbolize_names: true)
+        expected = {
+          errors: [
+            detail: "'Something invalid' is not a valid status"
+          ]
+        }
+        expect(error).to eq(expected)
       end
     end
 
     context "given invalid customer or subscription data" do
-      xit "returns an error if customer doesn't exist" do
+      it "returns an error if customer doesn't exist" do
+        patch "/api/v1/customers/12324546/subscriptions/2", params: {status: "Cancelled"}
 
+        expect(response).to_not be_successful 
+        expect(response.status).to eq(404)
+        error = JSON.parse(response.body, symbolize_names: true)
+        expected = {
+          errors: [
+            detail: "Couldn't find Customer with 'id'=12324546"
+          ]
+        }
+        expect(error).to eq(expected)
       end
 
-      xit "returns an error if subscription doesn't exist" do
+      it "returns an error if subscription doesn't exist" do
+        load_test_data
 
+        patch "/api/v1/customers/#{@sally.id}/subscriptions/223456", params: {status: "Cancelled"}
+
+        expect(response).to_not be_successful 
+        expect(response.status).to eq(404)
+        error = JSON.parse(response.body, symbolize_names: true)
+        expected = {
+          errors: [
+            detail: "Couldn't find Subscription with 'id'=223456"
+          ]
+        }
+        expect(error).to eq(expected)
       end
     end
   end
